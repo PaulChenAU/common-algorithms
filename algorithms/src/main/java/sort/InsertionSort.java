f54d843b41d61d909b61205f5805ab3640a1458e
BoyLondon <fucshu@163.com>
2020-08-29 15:31:23 +1000

    add insertion sort

---
+++ algorithms/src/main/java/sort/InsertionSort.java
@@ -0,0 +1,34 @@
+package sort;
+
+import static sort.Utils.swap;
+import static sort.Utils.printArr;
+
+public class InsertionSort {
+
+    /**
+     * iterate over array 1, 2, 3, ...
+     * key equals to arr[1, 2, 3...]
+     * arr[j+1] = arr[j] find the insert location and insert
+     * @param arr
+     */
+    public static void insertionSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int j = i - 1;
+            int key = arr[i];
+
+            while (j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,2,6,1,7,4,5};
+        printArr(arr, arr.length);
+        insertionSort(arr);
+        printArr(arr, arr.length);
+
+    }
+}
